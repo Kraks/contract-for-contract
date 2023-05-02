9140e74be775f8f8e1f0d44aae1258d4358b51ef
Guannan Wei <guannanwei@outlook.com>
2023-05-01 21:17:45 -0400

    make antlr workflow work

--- ConSol/src/bin/consol-test.ts
+++ ConSol/src/bin/consol-test.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
 import { CharStream, CommonTokenStream }  from 'antlr4';
-import SpecLexer from '../parser/SpecLexer';
-import SpecParser from '../parser/SpecParser';
+import SpecLexer from '../parser/SpecLexer.js';
+import SpecParser from '../parser/SpecParser.js';
 import { isNumber } from '../index';
 
 function main() {
-  const input = "3 + 4"
+  const input = "3+4\n"
+  console.log(input)
   const chars = new CharStream(input); // replace this with a FileStream as required
   const lexer = new SpecLexer(chars);
   const tokens = new CommonTokenStream(lexer);
