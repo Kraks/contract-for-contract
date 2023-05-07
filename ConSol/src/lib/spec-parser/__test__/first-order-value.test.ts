d8a7b29a34462dd04f004d51932f9683f87e7a65
William Aaron Cheung <troublor@live.com>
2023-05-07 17:33:40 -0400

    :white_check_mark: add tests

--- ConSol/src/lib/spec-parser/__test__/first-order-value.test.ts
+++ ConSol/src/lib/spec-parser/__test__/first-order-value.test.ts
@@ -29,7 +29,7 @@ describe('first-order value spec', () => {
       '{x | 1 + 2}',
       '{ (x) | 1 + 2}',
       '{ (x, y) | 1 + 2}',
-      // '{ {value: 5} (x, y) | 1 + 2}', // syntax error: mismatched input '5' expecting IDENT
+      '{ {value: 5} (x, y) | 1 + 2}',
     ];
     specs.forEach((specStr) =>
       it(`should parse ${specStr}`, () => {
