492b687d98d63daa6f3e3fef932feace57940e96
Zhuo Zhang <zhan3299@purdue.edu>
2023-05-14 20:36:57 -0400

    fix test cases [WIP]

--- ConSol/src/spec/__test__/temporal-contract.test.ts
+++ ConSol/src/spec/__test__/temporal-contract.test.ts
@@ -17,80 +17,175 @@ describe('temporal contract', () => {
     expect(spec_).toEqual(spec);
   });
 
+  it('single argument without conditions', () => {
+    const s = '{ f(x) returns (y) ~> g() returns z }';
+    const spec: TempSpec<string> = {
+      call1: { funName: 'f', kwargs: [], args: ['x'], rets: ['y'] },
+      call2: { funName: 'g', kwargs: [], args: [], rets: ['z'] },
+      conn: 2,
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
+  it('single argument with pre-condition', () => {
+    const s = `
+    { 
+        f(x) returns (y) ~> g() returns z
+        when {
+            x + y == z
+        }
+    }
+    `;
+    const spec: TempSpec<string> = {
+      call1: { funName: 'f', kwargs: [], args: ['x'], rets: ['y'] },
+      call2: { funName: 'g', kwargs: [], args: [], rets: ['z'] },
+      conn: 2,
+      preCond: 'x+y==z',
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
+  it('single argument with post-condition', () => {
+    const s = `
+    { 
+        f(x) returns (y) ~> g() returns z
+        ensures {
+            x + y == z
+        }
+    }
+    `;
+    const spec: TempSpec<string> = {
+      call1: { funName: 'f', kwargs: [], args: ['x'], rets: ['y'] },
+      call2: { funName: 'g', kwargs: [], args: [], rets: ['z'] },
+      conn: 2,
+      postCond: 'x+y==z',
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
+  it('single argument in dict without conditions', () => {
+    const s = '{ f{gas: g1}(x) ~/> g{gas: g2}() }';
+    const spec: TempSpec<string> = {
+      call1: {
+        funName: 'f',
+        kwargs: [{ fst: 'gas', snd: 'g1' }],
+        args: ['x'],
+        rets: [],
+      },
+      call2: {
+        funName: 'g',
+        kwargs: [{ fst: 'gas', snd: 'g2' }],
+        args: [],
+        rets: [],
+      },
+      conn: 3,
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
+  it('single argument in dict with pre-condition', () => {
+    const s = `
+    {
+        f{gas: g1}(x) ~/> g{gas: g2}()
+        when {
+            g1 > g2
+        }
+    }
+    `;
+    const spec: TempSpec<string> = {
+      call1: {
+        funName: 'f',
+        kwargs: [{ fst: 'gas', snd: 'g1' }],
+        args: ['x'],
+        rets: [],
+      },
+      call2: {
+        funName: 'g',
+        kwargs: [{ fst: 'gas', snd: 'g2' }],
+        args: [],
+        rets: [],
+      },
+      conn: 3,
+      preCond: 'g1>g2',
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
+  it('single argument in dict with post-condition', () => {
+    const s = `
+    {
+        f{gas: g1}(x) ~/> g{gas: g2}()
+        ensures {
+            g1 > g2
+        }
+    }
+    `;
+    const spec: TempSpec<string> = {
+      call1: {
+        funName: 'f',
+        kwargs: [{ fst: 'gas', snd: 'g1' }],
+        args: ['x'],
+        rets: [],
+      },
+      call2: {
+        funName: 'g',
+        kwargs: [{ fst: 'gas', snd: 'g2' }],
+        args: [],
+        rets: [],
+      },
+      conn: 3,
+      postCond: 'g1>g2',
+    };
+
+    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
+    expect(spec_).toEqual(spec);
+  });
+
   /*
-  it('single argument without side condition', () => {
-    const s = 'f{}(x) returns (y) ~> g() returns z';
-    const spec: TempSpec<string> = {
-      call1: { funName: 'f', kwargs: [], args: ['x'], rets: ['y'] },
-      call2: { funName: 'g', kwargs: [], args: [], rets: ['z'] },
-      conn: 2,
-    };
-
-    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
-    expect(spec_).toEqual(spec);
-  });
-
-  it('single argument with side condition', () => {
-    const s = 'f{}(x) returns (y) ~> g() returns z /\\ x + y == z';
-    const spec: TempSpec<string> = {
-      call1: { funName: 'f', kwargs: [], args: ['x'], rets: ['y'] },
-      call2: { funName: 'g', kwargs: [], args: [], rets: ['z'] },
-      conn: 2,
-      cond: 'x+y==z',
-    };
-
-    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
-    expect(spec_).toEqual(spec);
-  });
-
-  it('single argument in dict without side condition', () => {
-    const s = 'f{gas: gas}(x) ~/> g{gas: gas}()';
-    const spec: TempSpec<string> = {
-      call1: { funName: 'f', kwargs: [['gas', 'gas']], args: ['x'], rets: [] },
-      call2: { funName: 'g', kwargs: [['gas', 'gas']], args: [], rets: [] },
-      conn: 3,
-    };
-
-    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
-    expect(spec_).toEqual(spec);
-  });
-
-  it('single argument in dict with side condition', () => {
-    const s = 'f{gas: gas}(x) ~/> g{gas: gas}() /\\ f.gas > g.gas';
-    const spec: TempSpec<string> = {
-      call1: { funName: 'f', kwargs: [['gas', 'gas']], args: ['x'], rets: [] },
-      call2: { funName: 'g', kwargs: [['gas', 'gas']], args: [], rets: [] },
-      conn: 3,
-      cond: 'f.gas>g.gas',
-    };
-
-    const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
-    expect(spec_).toEqual(spec);
-  });
-
-  it('muliptles arguments with side condition (1)', () => {
-    const s =
-      'f{gas: gas, value: value}(x, y ,z) returns x ~/> g{}(a) returns (b, c, d) /\\ f.value + x > g.gas - a';
+  it('muliptles arguments with conditions (1)', () => {
+    const s = `
+    {
+        f{gas: g1, value: v1}(x, y, z) returns w
+            ~/> g(a) returns (b, c, d)
+        when {
+          v1 + x > g1 - w
+        }
+        ensures {
+            x + y + z == b + c + d
+        }
+    }
+    `
     const spec: TempSpec<string> = {
       call1: {
         funName: 'f',
         kwargs: [
-          ['gas', 'gas'],
-          ['value', 'value'],
+          {fst: 'gas', snd: 'g1'},
+          {fst: 'value', snd: 'v1'},
         ],
         args: ['x', 'y', 'z'],
-        rets: ['x'],
+        rets: ['w'],
       },
       call2: { funName: 'g', kwargs: [], args: ['a'], rets: ['b', 'c', 'd'] },
       conn: 3,
-      cond: 'f.value+x>g.gas-a',
+      preCond: 'v1+x>g1-w',
+      postCond: 'x+y+z==b+c+d',
     };
 
     const spec_ = CSSpecParse(s, visitor) as TempSpec<string>;
     expect(spec_).toEqual(spec);
   });
 
-  it('muliptles arguments with side condition (2)', () => {
+  it('muliptles arguments with conditions (2)', () => {
     const s =
       'f(x, y, z) returns (f1) => g{value: value, gas: gas, g: g}() /\\ 1 + 2';
     const spec: TempSpec<string> = {
