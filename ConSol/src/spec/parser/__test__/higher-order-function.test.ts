03519659d152b815ab03b36710d44779cdd89109
Xie Danning <xie342@purdue.edu>
2023-05-18 21:40:28 -0400

    fix failed test cases due to conflicts

--- ConSol/src/spec/parser/__test__/higher-order-function.test.ts
+++ ConSol/src/spec/parser/__test__/higher-order-function.test.ts
@@ -5,8 +5,8 @@ describe('higher-order functions spec', () => {
   describe('Parser', () => {
     const specs = [
       '{ f(x, g) requires { x > 0 } where { g(x) requires { x > 0 } } }',
-      `{ f(x, g) returns (h, y) requires { x > 0 } where { g(x) requires { x > 0 } }
-         ensures { y == 0 } where { h(z) returns r requires { z + r > 0 } ensures { z != 0 } }
+      `{ f(x, g) returns (h, y) requires { x > 0 } 
+         ensures { y == 0 } where { h(z) returns (r) requires { z + r > 0 } ensures { z != 0 } }
        }`,
     ];
     specs.forEach((specStr) =>
