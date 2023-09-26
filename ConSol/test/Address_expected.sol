414681985d25c710415ca2da844c296d456ca3fd
Guannan Wei <guannanwei@outlook.com>
2023-09-26 18:01:35 -0400

    example

--- ConSol/test/Address_expected.sol
+++ ConSol/test/Address_expected.sol
@@ -28,17 +28,46 @@ contract Caller {
         emit Response(y);
     }
 
-    function _callFooPre(address addr, uint x) private {
+    // Those parameter names should be the same as the specification
+    function _callFoo_pre(address addr, uint x) private {
         if (!(x>0)) revert preViolation("callFoo");
     }
 
-    function _dispatch_IReceiver_foo(uint256 addr, uint256 value, uint256 gas, string memory msg, uint x) private returns (uint) {
-        // TODO
-        return 0;
+    // Those parameter names should be the same as the specification
+    function _IReceiver_foo_spec0_pre(address addr, uint256 v, uint256 g, string memory mymsg, uint x) private {
+        if (!(v > 5 && g < 10000 && x != 0)) revert PreViolationAddr(1);
     }
 
-    function _attachSpec(uint256 addr, uint256 specId) private {
-        // TODO
+    // Those parameter names should be the same as the specification
+    function _IReceiver_foo_spec0_post(address addr, uint256 v, uint256 g, string memory mymsg, uint x, uint y) private {
+        if (!(y == x + 1)) revert PostViolationAddr(1);
+    }
+
+    // XXX: parameter names, should come from the interface
+    // XXX: we generate gas parameter by default, if the call site does not specify it, should use a default (max?) value
+    function _dispatch_IReceiver_foo(uint256 addr, uint256 value, uint256 gas, string memory mymsg, uint x) private returns (uint) {
+        uint96 specId = uint96(addr >> 160);
+        // Note: should only generate dispatching logic for IRerciever.foo for good
+        // 1 is the spec Id in this example
+        // 2^0, 2^1, 2^n, upto n = 95 in the worst case
+        if (specId & encodeNatSpecId(0) != 0) {
+            _IReceiver_foo_spec0_pre(_unwrap(addr), value, gas, mymsg, x);
+        }
+        uint y = IReceiver(_unwrap(addr)).foo{value: value, gas: gas}(mymsg, x);
+        if (specId & encodeNatSpecId(0) != 0) {
+            _IReceiver_foo_spec0_post(_unwrap(addr), value, gas, mymsg, x, y);
+        }
+        return y;
+    }
+
+    // Should be inlined/applied at compile-time
+    function _attachSpec(uint256 addr, uint96 specId) pure private returns (uint256) {
+        return addr | (specId << 160);
+    }
+
+    // Should be eliminated/applied at compile-time
+    function encodeNatSpecId(uint96 specId) pure private returns (uint96) {
+        return uint96(1 << specId);
     }
 
     function callFoo_guard(uint256 _addr, uint x) private {
@@ -47,17 +76,20 @@ contract Caller {
         // (eg compare equality), which would require unwrap them. So generally, we need to be able to parse
         // the specification expression and insert proper unwrap inside, in contrast to treating them as strings.
         // Right now, we don't have that yet, so let's just pass unwrapped addr to pre/post check functions.
-        _callFooPre(_unwrap(_addr), x);
-        _attachSpec(_addr, 1);
+        _callFoo_pre(_unwrap(_addr), x);
+        _addr = _attachSpec(_addr, encodeNatSpecId(0));
         callFoo_original(_addr, x);
     }
 
-    function _wrap(address payable addr) private returns (uint256) {
+    // Should be inlined/applied at compile-time
+    function _wrap(address payable addr) pure private returns (uint256) {
         return uint256(uint160(address(addr)));
     }
 
-    function _unwrap(uint256 guardedAddr) private returns (address) {
-        return address(uint160(guardedAddr));
+    // Should be inlined/applied at compile-time
+    function _unwrap(uint256 guardedAddr) pure private returns (address payable) {
+        //address a = payable(address(uint160(guardedAddr)));
+        return payable(address(uint160(guardedAddr)));
     }
 
     function callFoo(address payable _addr, uint x) public payable {
