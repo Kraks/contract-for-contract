cc189bd3e486aa245617b18bafce1590b523a6a8
DNXie <xie342@purdue.edu>
2024-02-27 02:55:21 -0500

    update test

---
+++ ConSol/test/testCallFoo.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.9;
+contract Caller {
+  event Response(bool success, bytes data);
+
+  // Let's imagine that contract Caller does not have the source code for the
+  // contract Receiver, but we do know the address of contract Receiver and the function to call.
+
+  /// @custom:consol { testCallFoo(addr, x) requires {x > 0} where { addr{value: v, gas: g}(mymsg, x) returns (flag, data) requires { v > 5 && g < 10000 && x != 0 } ensures { flag == true } } { addr.send(x) returns (b) requires { x < 1024 } ensures {b == true} }}
+  function testCallFoo(address payable _addr, uint256 x) public payable {
+    // You can send ether and specify a custom gas amount
+
+    (bool success, bytes memory data) = _addr.call{value: msg.value, gas: 5000}(
+      abi.encodeWithSignature("foo(string, uint256)", "call foo", x)
+    );
+
+    bool res = _addr.send(x);
+
+    emit Response(success, data);
+  }
+
+  // This one doesn't have value and gas
+
+  /// @custom:consol { anotherTest(addr, x) requires {x > 0} where { addr(mymsg, x) returns (flag, data) requires { v > 5 && g < 10000 && x != 0 } ensures { flag == true } }}
+  function anotherTest(address payable _addr, int256 x) public payable {
+    (bool success, bytes memory data) = _addr.call(
+      abi.encodeWithSignature("foo(string, uint256)", "call foo", 456)
+    );
+    emit Response(success, data);
+  }
+
+}
--- ConSol/test/testCallFoo_expected.sol
+++ ConSol/test/testCallFoo_expected.sol
@@ -13,8 +13,8 @@ contract Caller {
 
     /// @custom:consol { testCallFoo(addr, x) requires {x > 0} where { addr{value: v, gas: g}(mymsg, x) returns (flag, data) requires { v > 5 && g < 10000 && x != 0 } ensures { flag == true } } { addr.send(x) returns (b) requires { x < 1024 } ensures {b == true} }}
     function testCallFoo_original(address payable _addr, uint256 x) private payable {
-        (bool success, bytes memory data) = guarded_testCallFoo_addr(_addr, msg.value, 5000, "call foo", x);
-        bool res = guarded_testCallFoo_addr(_addr, x);
+        (bool success, bytes memory data) = guarded_testCallFoo_addr_Call(_addr, msg.value, 5000, "call foo", x);
+        bool res = guarded_testCallFoo_addrSend(_addr, x);
         emit Response(success, data);
     }
 
@@ -22,33 +22,33 @@ contract Caller {
         if (!x>0) revert preViolation("testCallFoo");
     }
 
-    function _addrPre(uint256 v, uint256 g, string memory mymsg,  uint256 x) private {
+    function _addrCallPre(uint256 v, uint256 g, string memory mymsg,  uint256 x) private {
         if (!v>5&&g<10000&&x!=0) revert PreViolationAddr(0);
     }
 
-    function _addrPost(uint256 v, uint256 g, string memory mymsg,  uint256 x, bool flag, bytes memory data) private {
+    function _addrCallPost(uint256 v, uint256 g, string memory mymsg,  uint256 x, bool flag, bytes memory data) private {
         if (!flag==true) revert PostViolationAddr(0);
     }
 
-    function guarded_testCallFoo_addr(address addr, uint256 v, uint256 g, string memory mymsg,  uint256 x) public payable returns (bool flag, bytes memory data) {
-        _addrPre(v, g, mymsg, x);
+    function guarded_testCallFoo_addr_Call(address addr, uint256 v, uint256 g, string memory mymsg,  uint256 x) public payable returns (bool flag, bytes memory data) {
+        _addrCallPre(v, g, mymsg, x);
         (bool flag, bytes memory data) = addr.call{value: v, gas: g}(abi.encodeWithSignature("foo(string, uint256)", mymsg, x));
-        _addrPost(v, g, mymsg, x, flag, data);
+        _addrCallPost(v, g, mymsg, x, flag, data);
         return (flag, data);
     }
 
-    function _addrPre(uint256 x) private {
+    function _addrSendPre(uint256 x) private {
         if (!x<1024) revert PreViolationAddr(0);
     }
 
-    function _addrPost(uint256 x, bool b) private {
+    function _addrSendPost(uint256 x, bool b) private {
         if (!b==true) revert PostViolationAddr(0);
     }
 
-    function guarded_testCallFoo_addr(address addr, uint256 x) public payable returns (bool b) {
-        _addrPre(x);
+    function guarded_testCallFoo_addr_Send(address addr, uint256 x) public payable returns (bool b) {
+        _addrSendPre(x);
         bool b = addr.send(x);
-        _addrPost(x, b);
+        _addrSendPost(x, b);
         return (b);
     }
 
