b5eb563e85e680a7476153242e071d818e9414e6
DNXie <xie342@purdue.edu>
2024-04-06 22:44:52 -0400

    todo: attach spec id to storage spec

--- ConSol/test/Address_storage.sol
+++ ConSol/test/Address_storage.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 interface IReceiver {
   function foo(string memory _message, uint _x) external payable returns (uint);
+  function bar(string memory _message, uint _x) external payable returns (uint);
 }
 
 contract Caller {
@@ -19,7 +20,7 @@ contract Caller {
   /// @custom:consol {IReceiver(testaddr).foo{value: v, gas: g}(mymsg, x) returns (y) requires { v==100 } ensures { data == true }}
   address testaddr;
 
-  /// @custom:consol {IReceiver(testaddr2).foo{value: v, gas: g}(mymsg, x) returns (y) requires { v==200 }}
+  /// @custom:consol {IReceiver(testaddr2).bar{value: v, gas: g}(mymsg, x) returns (y) requires { v==200 }}
   address testaddr2 = 0x86392dC19c0b719886221c78AB11eb8Cf5c52812;
 
   function callFoo(uint x) public payable returns (uint) {
@@ -27,7 +28,7 @@ contract Caller {
     // uint y = IReceiver(testaddr).foo("call foo", x);
     uint z = IReceiver(testaddr).foo("call foo", x);
     uint y = IReceiver(testaddr).foo{value: msg.value, gas: 5000}("call foo", x);
-    (bool success, )  = testaddr2.call(abi.encodeWithSignature("foo(uint256)", x));
+    (bool success, )  = testaddr2.call(abi.encodeWithSignature("bar(uint256)", x));
     // You can send ether and specify a custom gas amount
     
 
