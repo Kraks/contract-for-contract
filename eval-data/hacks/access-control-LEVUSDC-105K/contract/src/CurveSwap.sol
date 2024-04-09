3f752e6be9c22c53be9da5b1a34ef17f08e20160
Guannan Wei <guannanwei@outlook.com>
2024-04-09 15:18:32 -0400

    fix consol syntax

--- eval-data/hacks/access-control-LEVUSDC-105K/contract/src/CurveSwap.sol
+++ eval-data/hacks/access-control-LEVUSDC-105K/contract/src/CurveSwap.sol
@@ -57,9 +57,10 @@ contract CurveSwap is CurveContractInterface{
         return receToken;
     }
 
-    /// @custom:consol
-    /// approveToken(token, spender, _amount) returns (ret)
-    ///     requires spender == QueryAddressProvider(2)
+    /// @custom:consol {
+    ///   approveToken(token, spender, _amount) returns (ret)
+    ///   requires { spender == QueryAddressProvider(2) }
+    /// }
     function approveToken(address token, address spender, uint _amount) public returns (bool) {
         IERC20(token).safeApprove(spender, _amount);
         return true;
