17120d2f16fa985dc6e9128c86a71ead4dfcb7e1
William Aaron Cheung <git@troublor.xyz>
2024-04-12 21:48:27 -0500

    result: access-control

--- eval-data/hacks/access-control-LEVUSDC-105K/contract/original.sol
+++ eval-data/hacks/access-control-LEVUSDC-105K/contract/original.sol
@@ -3130,10 +3130,7 @@ contract CurveSwap is CurveContractInterface{
         return receToken;
     }
 
-    /// @custom:consol
-    /// {approveToken(token, spender, _amount) returns (ret)
-    ///     requires {spender == QueryAddressProvider(2)}}
-    // XXX: consol fix
+    // vulnerable
     function approveToken(address token, address spender, uint _amount) public returns (bool) {
         IERC20(token).safeApprove(spender, _amount);
         return true;
