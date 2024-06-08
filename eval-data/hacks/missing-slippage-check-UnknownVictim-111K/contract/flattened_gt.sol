8dabc57f486abbdaf99971ca770cc5c84df5d19a
Guannan Wei <guannanwei@outlook.com>
2024-06-08 16:46:00 -0400

    minor tweak of spec/gt

--- eval-data/hacks/missing-slippage-check-UnknownVictim-111K/contract/flattened_gt.sol
+++ eval-data/hacks/missing-slippage-check-UnknownVictim-111K/contract/flattened_gt.sol
@@ -166,6 +166,9 @@ contract ExchangeBetweenPools is Ownable {
     address public to_bank;
     uint256 public minimum_amount;
 
+    /// @dev {
+    ///           PriceInterface(curve).exchange_underlying{value: v, gas: g}(x, y, camount, n)
+    ///           ensures { _exchange_underlying_post_condition(camount) } }
     uint256 public curve = _wrap_curve(CurveInterface(0xbBC81d23Ea2c3ec7e56D39296F0cbB648873a5d3).curve());
 
     constructor(address _from_bank, address _to_bank, uint256 _min_amount) public {
@@ -177,6 +180,11 @@ contract ExchangeBetweenPools is Ownable {
         (u1, s1) = ERC20TokenBankInterface(to_bank).token();
         usdt = IERC20(u1);
         minimum_amount = _min_amount;
+    }
+
+    function _exchange_underlying_post_condition(uint256 camount) view internal returns (bool) {
+        uint256 namount = usdt.balanceOf(address(this));
+        return (((camount * 95) / 100) < namount) && (((camount * 100) / 95) > namount);
     }
 
     function changeMinimumAmount(uint256 _new_amount) public onlyOwner() {
@@ -204,11 +212,6 @@ contract ExchangeBetweenPools is Ownable {
         uint256 _addr = uint256(uint160(addr));
         _addr = _addr | (uint96(1 << 20) << 160);
         return _addr;
-    }
-
-    function _exchange_underlying_post_condition(uint256 camount) view internal returns (bool) {
-      uint256 namount = usdt.balanceOf(address(this));
-      return camount * 95 / 100 < namount && camount * 100 / 95 > namount;
     }
 
     function _PriceInterface_exchange_underlying_20_post(address curve, uint256 v, uint256 g, int128 x, int128 y, uint256 camount, uint256 n) private {
